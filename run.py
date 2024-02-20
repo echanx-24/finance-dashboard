3b58dbbb21694f5726603af78bb05854d5af7b9b
Ethan <141688462+echanx-24@users.noreply.github.com>
2024-02-19 23:29:17 -0500

    menu

--- run.py
+++ run.py
@@ -7,13 +7,13 @@ connection = sql.connect("dashboard/financials.db")
 if __name__ == "__main__":
     st.set_page_config(layout="wide", page_title="Finance Dashboard")
     
-    # hide_streamlit_style = """
-    # <style>
-    # #MainMenu {visibility: hidden;}
-    # footer {visibility: hidden;}
-    # </style>
-    # """
-    # st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+    hide_streamlit_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    </style>
+    """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
     st.title("Finance Dashboard")
     st.divider()
